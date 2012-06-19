d232ff5ffd7c822bcbe92e926e65dfe15d1b6290
Brian Corrigan <brian@agoragames.com>
2012-06-19 11:00:54 -0400

    Adds nokogiri require to validated_feedback model.  Hopefully fixes deserialization issue.

--- app/models/validated_feedback.rb
+++ app/models/validated_feedback.rb
@@ -1,4 +1,4 @@
-require 'base64'
+require 'nokogiri' #Needed in order to make deserializeation of nested objects work @BJC
 
 class ValidatedFeedback < ActiveRecord::Base
   # attr_accessible :title, :body
